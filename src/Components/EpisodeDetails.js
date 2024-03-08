e6441671a971bb28fe5baac6d70c55bc1ef333aa
DEbu-17 <debksahu2017@gmail.com>
2024-03-08 17:39:08 +0530

    some changes on episodes

--- src/Components/EpisodeDetails.js
+++ src/Components/EpisodeDetails.js
@@ -4,7 +4,7 @@ import Button from './SIgnUpComponents/Button'
 function EpisodeDetails({index,title,description,audioFile,onClick}) {
   return (
     <div style={{width:"95vw"}}>
-      <h2 style={{ textAlign: "left !important", width: "400px",marginTop:"-50px" }}>{index}. {title}</h2>
+      <h2 style={{textAlign:"left",marginLeft:"120px",marginTop:"-50px" }}>{index}. {title}</h2>
       <p style={{width:"80%",marginLeft:"162px", marginTop:"-25px"}}>{description}</p>
       <Button text="play" onClick={() => onClick(audioFile)} style={{width:"10%",marginLeft:"160px",marginBottom:"80px"}} />
     </div>
