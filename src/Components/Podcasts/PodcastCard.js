f041b3ea1ab2442dad544c5443a2b17634dbc115
DEbu-17 <debksahu2017@gmail.com>
2024-03-10 15:46:01 +0530

    implemented delete btn

--- src/Components/Podcasts/PodcastCard.js
+++ src/Components/Podcasts/PodcastCard.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import "../../Css files/podcast.css"
 import { Link } from 'react-router-dom';
+import { doc, deleteDoc, deleteField, updateDoc } from "firebase/firestore";
+import {auth, db, storage} from "../../FireBaseConfig"
+import { deleteObject, ref } from 'firebase/storage';
+import { toast } from 'react-toastify';
+import { IoIosCloseCircle } from "react-icons/io";
+
+
 function PodcastCard({id,title,smallImg}) {
+  
+console.log(Date.now());
+console.log(smallImg);
+ const deletePodcast = async () => {
+     try{
+       toast.success("podcast deleted permanently");
+       await deleteDoc(doc(db, "podcasts", id));
+
+       const cityRef = doc(db, "podcasts", id);
+
+       await updateDoc(cityRef, {
+         [id]: deleteField(),
+       });
+       
+     }catch(err){
+      console.log(err);
+     }
+  }
+
   return (
     <Link to={`/podcast/${id}`}>
       <div className="podcast-card">
-        <img className='small-img' src={smallImg} />
-        <p className='title-podcast'>{title}</p>
+        <img className="small-img" src={smallImg} />
+        <div onClick={deletePodcast} className='close-icon'>
+          <IoIosCloseCircle />
+        </div>
+        <p className="title-podcast">{title}</p>
       </div>
     </Link>
   );
